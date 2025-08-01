b7c15e0e128a366579ca7aa9755fa2e537814bf4
sushimura <zhengcunliaotailang@gmail.com>
2025-08-02 01:27:00 +0900

    feat: Spring Bootプロジェクトの初期セットアップ

---
+++ src/main/java/com/example/bookstore_ec_backend/BookstoreEcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstore_ec_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreEcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreEcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookstore_ec_backend/BookstoreEcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstore_ec_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreEcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
