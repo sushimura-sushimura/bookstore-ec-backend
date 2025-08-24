a9ed6994e74b7f0caab5ea1e6bf7284bfe906b1b
sushimura <zhengcunliaotailang@gmail.com>
2025-08-24 21:31:10 +0900

    feat(Book): BookRepositoryをJpaRepositoryから独立させ、BookMapperを追加

--- src/main/java/com/example/bookstore_ec_backend/domain/repository/BookRepository.java
+++ src/main/java/com/example/bookstore_ec_backend/domain/repository/BookRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-
+public interface BookRepository {
+  Optional<BookEntity> findById(Long bookId);
 }
