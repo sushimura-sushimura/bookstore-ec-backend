6d07b8fe1e884eb21e8536211bbbf8000ceb1892
sushimura <zhengcunliaotailang@gmail.com>
2025-08-24 23:00:23 +0900

    feat(repository): JpaRepositoryアノテーションを削除し、@Repositoryを削除

--- src/main/java/com/example/bookstore_ec_backend/domain/repository/BookRepository.java
+++ src/main/java/com/example/bookstore_ec_backend/domain/repository/BookRepository.java
@@ -1,11 +1,9 @@
 package com.example.bookstore_ec_backend.domain.repository;
 
 import com.example.bookstore_ec_backend.domain.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface BookRepository {
   Optional<BookEntity> findById(Long bookId);
 }
