61d87e2a32a3db7e904473eeb1190acf464ce82b
sushimura <zhengcunliaotailang@gmail.com>
2025-08-24 22:18:36 +0900

    feat(MyBatis): MyBatisBookRepositoryに必要なインポートを追加

--- src/main/java/com/example/bookstore_ec_backend/infrastructure/repository/MyBatisBookRepository.java
+++ src/main/java/com/example/bookstore_ec_backend/infrastructure/repository/MyBatisBookRepository.java
@@ -1,9 +1,12 @@
 package com.example.bookstore_ec_backend.infrastructure.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.example.bookstore_ec_backend.domain.repository.BookRepository;
 import com.example.bookstore_ec_backend.infrastructure.mybatis.BookMapper;
+import com.example.bookstore_ec_backend.domain.entity.BookEntity;
 
 import lombok.RequiredArgsConstructor;
 
