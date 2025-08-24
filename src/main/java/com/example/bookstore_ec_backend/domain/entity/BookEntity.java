5188c6448fdc1bceb8bf1fe54c87cdc69c98694b
sushimura <zhengcunliaotailang@gmail.com>
2025-08-24 23:00:19 +0900

    feat(entity): BookEntityからJPAアノテーションを削除

--- src/main/java/com/example/bookstore_ec_backend/domain/entity/BookEntity.java
+++ src/main/java/com/example/bookstore_ec_backend/domain/entity/BookEntity.java
@@ -1,27 +1,17 @@
 package com.example.bookstore_ec_backend.domain.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
     private String title;
     
-    @Column(nullable = false, length = 100)
     private String author;
     // 追加のフィールドやメソッドが必要であればここに記述
 }
