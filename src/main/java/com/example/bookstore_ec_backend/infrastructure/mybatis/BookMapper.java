b8554bab5cf68a5658d79e50f7227f5e5e8412a7
sushimura <zhengcunliaotailang@gmail.com>
2025-08-24 21:31:26 +0900

    feat(MyBatis): BookMapperインターフェースを追加し、findByIdメソッドを定義

---
+++ src/main/java/com/example/bookstore_ec_backend/infrastructure/mybatis/BookMapper.java
@@ -0,0 +1,11 @@
+package com.example.bookstore_ec_backend.infrastructure.mybatis;
+
+import com.example.bookstore_ec_backend.domain.entity.BookEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper // このインターフェースがMyBatisのMapperであることを示す
+public interface BookMapper {
+    Optional<BookEntity> findById(Long bookId);
+}
